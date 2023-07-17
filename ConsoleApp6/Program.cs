f88139059e6483836b7885efd224fae5bf4fe9da
Joseph Manangan <jmanangan@3cloudsolutions.com>
2023-07-18 02:57:53 +0800

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> argumentsDictionary = new Dictionary<string, string>();
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (!args[i].StartsWith("-") || !args[i].Contains(":"))
+            {
+                Console.WriteLine($"Invalid argument format: {args[i]}. Please use the format '-Key:Value'.");
+                return;
+            }
+
+            string[] keyValue = args[i].Split(':');
+            string key = keyValue[0].TrimStart('-');
+            string value = keyValue[1];
+
+            argumentsDictionary[key] = value;
+        }
+
+        // Display the dictionary contents
+        Console.WriteLine("Arguments Dictionary:");
+        foreach (var kvp in argumentsDictionary)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}
